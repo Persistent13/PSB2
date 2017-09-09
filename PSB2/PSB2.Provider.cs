c0ecdc61af4e788122836051213e2084612c1cbd
Dakota Clark <dakota@allclarks.com>
2017-09-09 15:58:08 -0700

    Base for development ready.

---
+++ PSB2/PSB2.Provider.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+using System.Collections.ObjectModel;
+
+namespace PSB2.Provider
+{
+    [CmdletProvider("B2", ProviderCapabilities.Credentials)]
+    public class B2Provider : NavigationCmdletProvider, IContentCmdletProvider, IPropertyCmdletProvider
+    {
+        public void ClearContent(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ClearContentDynamicParameters(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearProperty(string path, Collection<string> propertyToClear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ClearPropertyDynamicParameters(string path, Collection<string> propertyToClear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContentReader GetContentReader(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetContentReaderDynamicParameters(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContentWriter GetContentWriter(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetContentWriterDynamicParameters(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetProperty(string path, Collection<string> providerSpecificPickList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetPropertyDynamicParameters(string path, Collection<string> providerSpecificPickList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProperty(string path, PSObject propertyValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object SetPropertyDynamicParameters(string path, PSObject propertyValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsValidPath(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PSB2/PSB2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PSB2
+{
+    [Cmdlet(VerbsCommunications.Connect, "B2Cloud")]
+    [CmdletBinding(ConfirmImpact = ConfirmImpact.None)]
+    public class ConnectB2CloudCommand : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
